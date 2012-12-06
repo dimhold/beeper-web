aa78d3423d1dfd2d405c31a186617b78edf60956
dimhold <dimhold@gmail.com>
2012-12-06 22:46:44 +0300

    Initial commit

---
+++ src/main/sql/init.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
