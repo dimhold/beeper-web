ff5d821b430c2905e1f71793333773c4b96a67ce
by-ktif <monovar@tut.by>
2012-12-08 09:00:41 -0430

    Spring + JPA + Hibernate configuration done Java model implemented

---
+++ src/main/java/com/eucsoft/beeper/model/Channel.java
@@ -0,0 +1,35 @@
+package com.eucsoft.beeper.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "channel")
+public class Channel {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="language")
+	private String language;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+}
