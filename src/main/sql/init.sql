49b1a3b9dd6d5060f6b8a6e09ac7d51fffb1b2fb
by-ktif <monovar@tut.by>
2012-12-08 14:52:50 -0430

    FKs added for `user` and `message` tables

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -3,20 +3,12 @@ CREATE DATABASE beeper;
 
 USE beeper;
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `channel_id` varchar(255) DEFAULT NULL,
-  `account_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS `channel`;
 CREATE TABLE `channel` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `language` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `account`;
@@ -25,17 +17,30 @@ CREATE TABLE `account` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `channel_id` int(10) unsigned DEFAULT NULL,
+  `account_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_user_channel` (`channel_id`),
+  KEY `fk_user_account` (`account_id`),
+  CONSTRAINT `fk_user_channel` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `channel_id` int(10) unsigned NULL ,
-  `user_id` int(10) unsigned NULL ,
-  `created_on` datetime NULL ,
-  `location` varchar(127) NULL ,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-
-
+  `channel_id` int(10) unsigned NULL,
+  `user_id` int(10) unsigned NULL,
+  `created_on` datetime NULL,
+  `location` varchar(127) NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_message_channel` (`channel_id`),
+  KEY `fk_message_user` (`user_id`),
+  CONSTRAINT `fk_message_channel` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
