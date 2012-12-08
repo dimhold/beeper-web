bd280101ae9bae33ab77cfe3f248b9e693fa3ac7
dimhold <dimhold@gmail.com>
2012-12-08 13:39:16 +0300

    Add db creation

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE beeper 
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -12,6 +14,15 @@ CREATE TABLE `user` (
   `language` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `accaunt`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `language` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
 
 
 
