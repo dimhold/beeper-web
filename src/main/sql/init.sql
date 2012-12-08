7bac64963a3ab428abf5ad8f5730233f44c01de6
dimhold <dimhold@gmail.com>
2012-12-08 13:49:15 +0300

    Add database creation

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -1,25 +1,29 @@
-CREATE DATABASE beeper 
+DROP DATABASE beeper;
+CREATE DATABASE beeper; 
+
+USE beeper;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `channel_id` varchar(255) DEFAULT NULL,
+  `account_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `channel`;
-CREATE TABLE `user` (
+CREATE TABLE `channel` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `language` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `accaunt`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `accoun`;
+CREATE TABLE `account` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `language` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
