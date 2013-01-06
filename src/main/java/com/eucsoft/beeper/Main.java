bc42b07ca60c9f7fee1e8535a2cb3484cb6edade
dimhold <dimhold@gmail.com>
2013-01-06 17:35:44 +0300

    Add Main class

---
+++ src/main/java/com/eucsoft/beeper/Main.java
@@ -0,0 +1,14 @@
+package com.eucsoft.beeper;
+
+import com.eucsoft.beeper.server.Server;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Server.start();
+	}
+
+}
